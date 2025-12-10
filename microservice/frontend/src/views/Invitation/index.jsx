92a18f6d330dab8e3f2d5f52658563f6c9017009
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2025-12-10 15:56:02 +0700

    Update index.jsx

--- microservice/frontend/src/views/Invitation/index.jsx
+++ microservice/frontend/src/views/Invitation/index.jsx
@@ -1,38 +1,25 @@
 //@ts-check
 
-import { Loader } from "@mantine/core";
-import { useUser } from "../../context/useUser";
 import useInvitation from "./hooks";
 import FormInvitation from "./FormInvitation";
-import { Navigate } from "react-router-dom";
 
 const InvitationPage = () => {
-
-    const { user, isLoading } = useUser();
 
     const {
         isSubmitting,
         handleValidateInvitaiton
     } = useInvitation()
 
-    if (!user) {
-        return (
-            <>
-                <div className="flex justify-center items-center h-screen px-4">
-                    {
-                        isLoading ?
-                            <Loader /> :
-                            <FormInvitation
-                                isSubmitting={isSubmitting}
-                                onSubmit={handleValidateInvitaiton}
-                            />
-                    }
-                </div>
-            </>
-        );
-    }
-
-    return <Navigate to="/dashboard" replace />;
+    return (
+        <>
+            <div className="flex justify-center items-center h-screen px-4">
+                <FormInvitation
+                    isSubmitting={isSubmitting}
+                    onSubmit={handleValidateInvitaiton}
+                />
+            </div>
+        </>
+    );
 }
 
 export default InvitationPage
